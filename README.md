9b3eb742d452a86521b3d49efcb80253b3579401
Saba Khan <125646083+githubsaba@users.noreply.github.com>
2024-05-26 18:57:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+To solve chromadb could not build wheel issue used this command
+####conda install -c conda-forge chromadb####
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI source code analysis project',
+    version= '0.0.0',
+    author= 'Saba Khan',
+    author_email= 'ssaba0611@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
